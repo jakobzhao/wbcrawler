d1b16a0e12fe63952f5d34cde3bf0d3c3679d96b
bo <bo_zhao@hks.harvard.edu>
2015-10-07 14:51:33 -0400

    0.0.5

--- src/crawler.py
+++ src/crawler.py
@@ -8,18 +8,25 @@ Created on Oct 4, 2015
 @organization: Harvard Kennedy School
 '''
 
+import datetime
 
 from utils import parse_keyword, weibo_login
 
+start = datetime.datetime.now()
 
+# ProjectName
+project_name = "weibo"
 #Variables
-keyword = '人生'
+keyword = '奇怪'
 
 pages = 1
 
 br = weibo_login()
-parse_keyword(keyword, br, project="weibo")
+parse_keyword(keyword, br, project_name)
 
+end = datetime.datetime.now()
+
+print "This program costs %d seconds." % (end - start).seconds
 
 print "all finished"
 

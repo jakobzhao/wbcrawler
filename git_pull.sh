c3b37c59487d960e3e3b97aea2db98aebdbaa3f0
bo <bo_zhao@hks.harvard.edu>
2015-10-24 11:17:51 -0400

    2.8.7 WindowsError Test

---
+++ git_pull.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# free_mememory.sh
+# insurance data collection program
+# Oct. 22, 2015
+# Harvard Kennedy School
+# Bo Zhao
+# bo_zhao@hks.harvard.edu
+cd /home/bo/Workspace/wbcrawler
+sudo git checkout --force
+sudo git pull
+echo "the program has updated to the latest version"

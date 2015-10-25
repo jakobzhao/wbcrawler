d2373968b53e087be510b48b294ddaeb4e8e8e7a
bo <bo_zhao@hks.harvard.edu>
2015-10-25 01:40:21 -0400

    2.9.5 modify the parse_repost func

--- wbcrawler/database.py
+++ wbcrawler/database.py
@@ -82,4 +82,4 @@ def delete_post(mid, settings):
             delete_post(reply_mid, settings)
     else:
         return
-    log(NOTICE, "The specified post and its replies have been marked as {'deleted': true}.")
+    log(NOTICE, "The specified post and its replies have been marked as {'deleted': true}." % mid)

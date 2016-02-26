e3f598c74f8bab11e4b1f481bff1c86dce8d3653
bo <bo_zhao@hks.harvard.edu>
2016-02-25 19:10:25 -0500

    2.13 adding more robots

--- cga/settings.py
+++ cga/settings.py
@@ -8,17 +8,17 @@
 # @organization: Harvard Kennedy School
 
 SETTINGS = {
-               'project': 'cga',
-               'pis': ['bo_zhao@hks.harvard.edu'],
-               'robot_table': 'accounts',
-               'address': '127.0.0.1',
-               'port': 27017,
-               'robot_num': 1,
-               'control_days': 9999,
-               'keywords': [],
-               'd_types': [{'1h': 9999}],
-               'replies_control_days': 9999,
-               'min_fwd_times': 2
+    'project': 'cga',
+    'pis': ['bo_zhao@hks.harvard.edu'],
+    'robot_table': 'accounts',
+    'address': '127.0.0.1',
+    'port': 27017,
+    'robot_num': 10,
+    'control_days': 9999,
+    'keywords': [],
+    'd_types': [{'1h': 9999}],
+    'replies_control_days': 9999,
+    'min_fwd_times': 2
 }
 
 SETTINGS2 = {
@@ -27,7 +27,7 @@ SETTINGS2 = {
     'robot_table': 'accounts',
     'address': '127.0.0.1',
     'port': 27017,
-    'robot_num': 1,
+    'robot_num': 10,
     'control_days': 9999,
     'keywords': [],
                 'd_types': [{u'社会': 4188}, {u'国际': 6288}, {u'科技': 2088}, {u'科普': 5988}, {u'数码': 5088}, {u'财经': 6388}, {u'股市': 1288}, {u'明星': 4288}, {u'综艺': 4688}, {u'电视剧': 2488}, {u'电影': 3288}],
@@ -41,7 +41,7 @@ SETTINGS3 = {
     'robot_table': 'accounts',
     'address': '127.0.0.1',
     'port': 27017,
-    'robot_num': 1,
+    'robot_num': 10,
     'control_days': 9999,
     'keywords': [],
                 'd_types': [{u'音乐': 5288}, {u'汽车': 5188}, {u'体育': 1388}, {u'运动': 4788}, {u'健康': 2188}, {u'廋身': 6488}, {u'养生': 6588}, {u'军事': 6688}, {u'历史': 6788}, {u'模特': 6288}, {u'美图': 4988}, {u'情感': 1988}],
@@ -50,17 +50,17 @@ SETTINGS3 = {
 }
 
 SETTINGS4 = {
-                'project': 'cga',
-                'pis': ['bo_zhao@hks.harvard.edu'],
-                'robot_table': 'accounts',
-                'address': '127.0.0.1',
-                'port': 27017,
-                'robot_num': 1,
-                'control_days': 9999,
-                'keywords': [],
-                'd_types': [{u'语录': 6888}, {u'笑话': 4388}, {u'辟谣': 4388}, {u'正能量': 7088}, {u'政务': 5788}, {u'游戏': 4888}, {u'旅游': 2588}, {u'育儿': 3188}, {u'教育': 1488}, {u'美食': 2688}, {u'房产': 5588}, {u'家居': 5888}],
-                'replies_control_days': 9999,
-                'min_fwd_times': 2
+    'project': 'cga',
+    'pis': ['bo_zhao@hks.harvard.edu'],
+    'robot_table': 'accounts',
+    'address': '127.0.0.1',
+    'port': 27017,
+    'robot_num': 10,
+    'control_days': 9999,
+    'keywords': [],
+    'd_types': [{u'语录': 6888}, {u'笑话': 4388}, {u'辟谣': 4388}, {u'正能量': 7088}, {u'政务': 5788}, {u'游戏': 4888}, {u'旅游': 2588}, {u'育儿': 3188}, {u'教育': 1488}, {u'美食': 2688}, {u'房产': 5588}, {u'家居': 5888}],
+    'replies_control_days': 9999,
+    'min_fwd_times': 2
 }
 
 SETTINGS5 = {
@@ -69,7 +69,7 @@ SETTINGS5 = {
     'robot_table': 'accounts',
     'address': '127.0.0.1',
     'port': 27017,
-    'robot_num': 1,
+    'robot_num': 10,
     'control_days': 9999,
     'keywords': [],
     'd_types': [{u'星座': 1688}, {u'读书': 4588}, {u'三农': 7188}, {u'设计': 5388}, {u'艺术': 5488}, {u'时尚': 4488}, {u'美妆': 1588}, {u'动漫': 2388}, {u'宗教': 5688}, {u'神最右': 6399}, {u'萌宠': 2788}],

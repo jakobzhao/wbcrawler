e2231ba2c7fbd25fda15fd58f2c8598ec8853152
bo <bo_zhao@hks.harvard.edu>
2016-05-15 21:52:24 -0400

    2.15 adding firefox path

--- philanthropy/settings.py
+++ philanthropy/settings.py
@@ -13,7 +13,7 @@ SETTINGS = {
     'robot_table': 'accounts',
     'address': '127.0.0.1',
     'port': 27017,
-    'robot_num': 3,
+    'robot_num': 5,
     'keywords': ['捐赠 元', '慈善'],
     'control_days': 19999,
     'replies_control_days': 19999,
